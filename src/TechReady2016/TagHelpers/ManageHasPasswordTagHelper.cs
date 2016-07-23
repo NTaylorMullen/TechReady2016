a51f6966a16a931c11003c653e4dc0a613c5bf64
N. Taylor Mullen <nimullen@microsoft.com>
2016-07-23 13:09:19 -0700

    Add ManageHasPasswordTagHelper.

---
+++ src/TechReady2016/TagHelpers/ManageHasPasswordTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TechReady2016.TagHelpers
+{
+    [OutputElementHint("a")]
+    public class ManageHasPasswordTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+
+        public ManageHasPasswordTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public bool Change { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var linkContent = Change ? "Change" : "Set";
+            output.Content.SetContent(linkContent);
+
+            var actionName = Change ? "ChangePassword" : "SetPassword";
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            var hrefValue = urlHelper.Action(actionName, "Manage");
+            output.Attributes.SetAttribute("href", hrefValue);
+
+            output.PreElement.SetContent("[ ");
+            output.PostElement.SetContent(" ]");
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
