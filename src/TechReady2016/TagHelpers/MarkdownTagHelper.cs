f57154541851aa7c5e2e4ac2377a4a0e74ff9e02
N. Taylor Mullen <nimullen@microsoft.com>
2016-07-23 13:24:02 -0700

    Add MarkdownTagHelper.

---
+++ src/TechReady2016/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonMark;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TechReady2016.TagHelpers
+{
+    [HtmlTargetElement("markdown")]
+    [HtmlTargetElement(Attributes = "markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+
+            var childContent = await output.GetChildContentAsync();
+            var lines = childContent.GetContent()
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line.Trim());
+            var content = string.Join(" ", lines);
+            var transformedContent = CommonMarkConverter.Convert(content);
+
+            output.Content.SetHtmlContent(transformedContent);
+
+            output.Attributes.RemoveAll("markdown");
+        }
+    }
+}
